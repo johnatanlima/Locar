fc0c9b0313fc2ad93fdbe7bf1f4ba15ea3d036de
Johnatan Lima <johw.23@outlook.com>
2020-04-13 01:22:01 -0300

    Adição de controller para usuarios.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Locar.AcessoDados.Interface;
+using Locar.Models;
+using Locar.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Locar.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ILogger _logger;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio, ILogger logger)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Registrar()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await _usuarioRepositorio.EfetuarLogout();
+
+                _logger.LogInformation("Solicitando pagina de registro..");
+
+                return View();
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistroViewModel registro)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = new Usuario()
+                {
+                    Nome = registro.Nome,
+                    Telefone = registro.Telefone,
+                    Cpf = registro.Cpf,
+                    Email = registro.Email,
+                    UserName = registro.NomeUsuario,
+                    PasswordHash = registro.Senha
+                };
+
+                _logger.LogInformation("Registrando novo usuario...");
+
+                var result = await _usuarioRepositorio.SalvarUsuario(usuario, registro.Senha);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("Registro feito com sucesso...");
+                    _logger.LogInformation("Definindo nivel de acesso de novo usuario..");
+
+                    var nivelAcesso = "Cliente";
+                    await _usuarioRepositorio.AtribuirNivelAcesso(usuario, nivelAcesso);
+                    _logger.LogInformation("Atribuicao ok..");
+
+                    _logger.LogInformation("Realizando login automatico pós registro...");
+                    await _usuarioRepositorio.EfetuarLogin(usuario, false);
+                        _logger.LogInformation("Novo usuario logado com sucesso...");
+
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    _logger.LogInformation("Não possivel realizar novo registro...");
+
+                    foreach(var erro in result.Errors)
+                    {
+                        ModelState.AddModelError("", erro.Description.ToString());
+
+
+                    }
+                }
+
+            }
+            _logger.LogInformation("Informações de usuario inválidas.");
+            //Se não válido model
+            return View(registro);
+        }
+    }
+}
---
+++ Models/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.Models.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [StringLength(100, ErrorMessage = "Máximo de 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [StringLength(11, ErrorMessage = "Máximo de 11 caracteres")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        //[StringLength(11, ErrorMessage = "Máximo de 11 caracteres")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [StringLength(32, ErrorMessage = "Máximo de 32 caracteres")]
+        public string NomeUsuario { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Insira um e-mail válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatorio")]
+        [DataType(DataType.Password)]
+        public string Senha{ get; set; }
+    }
+}
