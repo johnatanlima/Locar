8d706830f3836f896152bce6a97bc6f12a90b70a
Johnatan Lima <johw.23@outlook.com>
2020-04-26 22:09:00 -0300

    Data Annotations no model Endereço

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -1,20 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Locar.Models
 {
 
-public class Endereco
-{
-    public int EnderecoId { get; set; }
+    public class Endereco
+    {
+        public int EnderecoId { get; set; }
 
-    public string Estado { get; set; }
-    public string Cidade { get; set; }
+        [StringLength(45, ErrorMessage ="Limite de 45 caracteres.")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Estado { get; set; }
 
-    public string Bairro { get; set; }
+        [StringLength(45, ErrorMessage = "Limite de 45 caracteres.")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cidade { get; set; }
 
-    public string Rua { get; set; }
+        [StringLength(45, ErrorMessage = "Limite de 45 caracteres.")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Bairro { get; set; }
 
-    public string Numero { get; set; }
+        [StringLength(45, ErrorMessage = "Limite de 45 caracteres.")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Rua { get; set; }
 
-    public string UsuarioId { get; set; }
-    public Usuario UsuarioVirtual { get; set; }
-}
+        [Range(0, int.MaxValue, ErrorMessage ="Tamanho de dado excedido.")]
+        [Required(ErrorMessage ="Campo obrigatório")]
+        public string Numero { get; set; }
+
+        public string UsuarioId { get; set; }
+    
+        public Usuario UsuarioVirtual { get; set; }
+    }
 }
