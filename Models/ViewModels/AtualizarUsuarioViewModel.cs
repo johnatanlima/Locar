3b0d0e9b04421710a542b910223419730b125420
Johnatan Lima <johw.23@outlook.com>
2020-04-27 01:40:28 -0300

    Inserção de nova viewmodel para atualizar dados de usuario

---
+++ Models/ViewModels/AtualizarUsuarioViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+
+namespace Locar.Models.ViewModels
+{
+    public class AtualizarUsuarioViewModel
+    {
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [StringLength(45, ErrorMessage ="Máx de 45 caracteres")]
+        public string Nome { get; set; }
+
+        [StringLength(45, ErrorMessage = "Máx de 45 caracteres")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Cpf { get; set; }
+
+        [StringLength(45, ErrorMessage = "Máx de 45 caracteres")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Telefone { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage ="Verifique o e-mail")]
+        public string Email { get; set; }
+
+    }
+}
