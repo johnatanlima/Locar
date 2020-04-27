591e2d3c2a07f61c577f8913b03668cebdfcc12c
Johnatan Lima <johw.23@outlook.com>
2020-04-27 04:08:44 -0300

    Adição de repositório para gerenciar sobre saldos

---
+++ AcessoDados/Interface/IContaRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using Locar.AcessoDados.Repositorio;
+using Locar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.AcessoDados.Interface
+{
+    public interface IContaRepositorio : IRepositorioGenerico<Conta>
+    {
+        new Task<IEnumerable<Conta>> PegarTodos();
+
+        int PegarSaldoPeloId(string id);
+    }
+}
---
+++ AcessoDados/Repositorio/ContaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using Locar.AcessoDados.Interface;
+using Locar.Data;
+using Locar.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.AcessoDados.Repositorio
+{
+    public class ContaRepositorio : RepositorioGenerico<Conta>, IContaRepositorio
+    {
+        private readonly LocarDbContext _contexto;
+
+        public ContaRepositorio(LocarDbContext contexto) : base(contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public int PegarSaldoPeloId(string id)
+        {
+            return _contexto.Contas.FirstOrDefault(c => c.UsuarioId == id).Saldo;
+        }
+
+        public new async Task<IEnumerable<Conta>> PegarTodos()
+        {
+            return await _contexto.Contas.Include(c => c.UsuarioVirtual).ToListAsync();
+        }
+    }
+}
