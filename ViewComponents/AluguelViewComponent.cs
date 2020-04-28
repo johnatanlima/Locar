22ac6f4baa77b6d787aca5010184527481da365d
Johnatan Lima <johw.23@outlook.com>
2020-04-28 15:37:12 -0300

    Desenvolvimento do componente para mostrar carros-alugueis

---
+++ ViewComponents/AluguelViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Locar.AcessoDados.Interface;
+using Locar.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.ViewComponents
+{
+    public class AluguelViewComponent : ViewComponent
+    {
+        private readonly LocarDbContext _contexto;
+
+        public AluguelViewComponent(LocarDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int carroId)
+        {
+            
+            return View(await _contexto.Carros.FirstOrDefaultAsync(c => c.CarroId == carroId));
+        }
+    }
+}
