5b670237213d9240086bceef4c7591a86d32dae7
Johnatan Lima <johw.23@outlook.com>
2020-04-29 15:18:39 -0300

    Configurando classes e servico para envio de email

---
+++ Servicos/EmailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.Servicos
+{
+    public class EmailSender : IEmail
+    {
+        public Task EnviarEmail(string email, string assunto, string mensagem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Servicos/IEmail.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.Servicos
+{
+    public interface IEmail
+    {
+        Task EnviarEmail(string email, string assunto, string mensagem);
+    }
+}
