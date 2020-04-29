d80e79a49ca342b6683abafffd417ce271ae8f88
Johnatan Lima <johw.23@outlook.com>
2020-04-29 14:36:02 -0300

    Corrigido e funcionando as ViewComponent - Carros alugados

---
+++ ViewComponents/CarrosAlugadosViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Locar.AcessoDados.Interface;
+using Locar.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locar.ViewComponents
+{
+    public class CarrosAlugadosViewComponent : ViewComponent
+    {
+        private readonly LocarDbContext _contexto;
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+
+        public CarrosAlugadosViewComponent(LocarDbContext contexto, IUsuarioRepositorio usuarioRepositorio)
+        {
+            _contexto = contexto;
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var usuarioLogado = await _usuarioRepositorio.PegarUsuarioLogado(HttpContext.User);
+
+            return View(await _contexto.Alugueis.Include(a => a.CarroVirtual).Where(a => a.UsuarioId == usuarioLogado.Id).ToListAsync());
+        }
+    }
+}
