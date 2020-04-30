8a319304bd22b29b6457b505b4203806fef71786
Johnatan Lima <johw.23@outlook.com>
2020-04-29 23:57:01 -0300

    Configurando classe para envio de email

--- Servicos/ConfiguracaoEmail.cs
+++ Servicos/ConfiguracaoEmail.cs
@@ -7,6 +7,15 @@ namespace Locar.Servicos
 {
     public class ConfiguracaoEmail
     {
+        public string Endereco { get; set; }
+
+        public int Porta { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public string Destinatario { get; set; }
 
     }
 }
--- Servicos/EmailSender.cs
+++ Servicos/EmailSender.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Locar.Servicos
 {
     public class EmailSender : IEmail
     {
-        public Task EnviarEmail(string email, string assunto, string mensagem)
+        private ConfiguracaoEmail _configEmail;
+        public EmailSender(ConfiguracaoEmail configEmail)
         {
-            throw new NotImplementedException();
+            _configEmail = configEmail;
         }
+
+        public async Task EnviarEmail(string email, string assunto, string mensagem)
+        {
+            var destinatario = String.IsNullOrEmpty(email) ? _configEmail.Email : email;
+
+            MailMessage mailMessage = new MailMessage
+            {
+                From = new MailAddress(_configEmail.Email, "Nathan")
+            };
+
+            mailMessage.To.Add(new MailAddress(destinatario));
+            mailMessage.Subject = assunto;
+            mailMessage.Body = mensagem;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Priority = MailPriority.High;
+
+
+        }
+
     }
 }
